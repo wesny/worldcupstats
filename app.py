8d0192202c9b7e9ce96442b0ff829d8e96294f95
Andre Monteiro <andre.monteiro@yale.edu>
2017-12-02 13:53:45 -0500

    World Cup check by year

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from hashlib import sha512
 import os
 import psycopg2
@@ -24,5 +24,36 @@ def home():
 def world_map():
     return render_template("worldmap.html")
 
+@app.route("/worldcups", methods=['GET', 'POST'])
+def world_cup():
+	# print('World cup')
+
+	print(request.method)
+	if(request.args):
+
+		print(request.args['year'])
+
+		cur.execute("SELECT * FROM worldcup WHERE year = '"+str(request.args['year'])+"';")
+		records = cur.fetchall()
+		print(records)
+		# print(records['winner'])
+
+		if len(records) > 0:
+			print('Records')
+			records = records[0]
+			return render_template("worldcups.html", year=request.args['year'], winner=records[1], runnerup=records[2],third=records[3],fourth=records[4])
+		else:
+			print('No cup!')
+			return render_template("worldcups.html", year=request.args['year'], nocup=True)
+	else:
+		return render_template("worldcups.html", mystring='whee', myint=1)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
